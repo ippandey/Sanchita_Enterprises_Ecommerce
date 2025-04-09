b9f5899f5c447cac3cb4e28c2a72188179c0a8d6
Priyanshu <priyanshu.p2025@gmail.com>
2025-04-09 22:59:06 +0530

    Connected frontend to backend using env variable, fixed axios baseURL

--- Admin/src/services/api.js
+++ Admin/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/products";
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api/products`;
 
 // Add Product
 export const addProduct = async (product) => {
@@ -13,13 +13,9 @@ export const addProduct = async (product) => {
     }
   }
 
-  const response = await axios.post(
-    "http://localhost:5000/api/products",
-    formData,
-    {
-      headers: { "Content-Type": "multipart/form-data" },
-    }
-  );
+  const response = await axios.post(API_URL, formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
   return response.data;
 };
 
